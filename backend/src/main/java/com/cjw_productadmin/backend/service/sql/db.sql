7177fa71362efd13a44dbf6a077584435fa738cc
최주욱 <choijw9808@bnosoft.co.kr>
2023-11-09 16:55:48 +0900

    [backend] DB 수정에 따른 sql문 수정

--- backend/src/main/java/com/cjw_productadmin/backend/service/sql/db.sql
+++ backend/src/main/java/com/cjw_productadmin/backend/service/sql/db.sql
@@ -1,5 +1,7 @@
-create table admin (id int primary key, name varchar(40), deptname varchar(40));
-create table product (id int primary key, name varchar(40), detail varchar(255), price int);
+create table admin (id serial primary key, name varchar(40), deptname varchar(40));
+create table product (id serial primary key, name varchar(40), detail varchar(255), price int);
+
+create sequence id start with 1 increment by 1;
 
 insert into admin (id, name, deptname) values (1, '최주욱', 'SI사업본부');
 insert into admin (id, name, deptname) values (2, '김보성', 'SI사업본부');
